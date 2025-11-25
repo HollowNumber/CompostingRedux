4dc0da12816f9fe62ae3e817323f1268cace1142
Hollow <mikkel.helding@gmail.com>
2025-11-25 21:57:20 +0100

    Actually store the mod config

--- CompostingRedux/CompostingRedux/CompostingReduxModSystem.cs
+++ CompostingRedux/CompostingRedux/CompostingReduxModSystem.cs
@@ -1,7 +1,36 @@
-﻿using Vintagestory.API.Common;
+﻿using CompostingRedux.BlockEntities;
+using CompostingRedux.Blocks;
+using Vintagestory.API.Common;
 
 namespace CompostingRedux;
 
 public class CompostingReduxModSystem : ModSystem
 {
+    public static CompostConfig Config { get; private set; }
+        
+    public override void Start(ICoreAPI api)
+    {
+        base.Start(api);
+
+
+        try
+        {
+            Config = api.LoadModConfig<CompostConfig>("compostingreduxconfig.json") ?? CompostConfig.GetDefault();
+            api.Logger.Notification("CompostingRedux started successfully.");
+        }
+        catch
+        {
+            api.Logger.Error("CompostingRedux couldn't start.");
+            Config = CompostConfig.GetDefault();
+        }
+            
+        api.StoreModConfig(Config, "compostingreduxconfig.json");
+            
+        string modid = Mod.Info.ModID;
+            
+        api.RegisterBlockClass(modid + ".compostbinblock", typeof(BlockCompostBin)); 
+            
+        api.RegisterBlockEntityClass(modid + ".compostbinentity", typeof(BlockEntityCompostBin));
+            
+    }
 }
